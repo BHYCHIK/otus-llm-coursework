d30158ee161961f2ea2ae75e10eed6e339ccbe0e
Ivan Remen <bhyk@yandex.ru>
2026-03-30 10:47:40 +0300

    Fix

--- src/app.py
+++ src/app.py
@@ -18,11 +18,12 @@ from src.sentiment_detector.sentiment_detector import SentimentDetector
 
 load_dotenv('../.env', verbose=True)
 
-sentiment_detector = SentimentDetector('./../sentiment_bge_m3_ru_cls/prod')
+sentiment_detector = SentimentDetector(os.environ.get("SENTIMENT_DETECTION_MODEL"))
 
 class State(TypedDict):
     original_review: str
     fixed_review: str
+    sentiment: str
 
 llm = ChatOpenAI(
     base_url=os.getenv('BASEURL'),
@@ -55,19 +56,26 @@ def fix_review_call(state: State):
     response = llm.invoke([system_message, user_message])
     print(response.content)
 
-    sentiment = sentiment_detector.predict_sentiment([response.content])
-    print(sentiment)
-
     return {
         'fixed_review': response.content,
+    }
+
+def sentiment_detection_call(state: State):
+    print('Sentiment detection call')
+    sentiment = sentiment_detector.predict_sentiment([state['fixed_review']])[0]['label']
+    return {
+        'sentiment': sentiment,
     }
 
 
 workflow = StateGraph(State)
 workflow.add_node('fix_review', fix_review_call)
+workflow.add_node('sentiment_detection', sentiment_detection_call)
 
 workflow.add_edge(START, "fix_review")
-workflow.add_edge('fix_review', END)
+workflow.add_edge('fix_review', 'sentiment_detection')
+workflow.add_edge('sentiment_detection', END)
+
 
 app = workflow.compile(checkpointer=memory)
 
