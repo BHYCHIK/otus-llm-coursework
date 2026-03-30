2e81678f20c34137e1c6dcc9aed3a792e564d641
Ivan Remen <bhyk@yandex.ru>
2026-03-30 22:45:42 +0300

    metrics

--- src/app.py
+++ src/app.py
@@ -7,12 +7,13 @@ from langgraph.checkpoint.memory import MemorySaver
 from langgraph.constants import START, END
 from langgraph.graph import StateGraph
 from langfuse.langchain import CallbackHandler as LangfuseCallbackHandler
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 from dotenv import load_dotenv
 import os
 import uuid
 
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, Response
 
 from pydantic import BaseModel, Field
 
@@ -21,6 +22,16 @@ from src.sentiment_detector.sentiment_detector import SentimentDetector
 load_dotenv('.env')
 
 app = FastAPI()
+
+review_analysis_total = Counter(
+    "review_analysis_total",
+    "Total review analysis",
+    ['product_id', 'sentiment',
+     'good_speed_of_delivery', 'good_price', 'good_quality', 'good_good_looking', 'good_fit_description',
+     'good_functionality',
+     'bad_speed_of_delivery', 'bad_price', 'bad_quality', 'bad_good_looking', 'bad_fit_description',
+     'bad_functionality'],
+)
 
 sentiment_detector = SentimentDetector(os.environ.get("SENTIMENT_DETECTION_MODEL"))
 
@@ -196,8 +207,13 @@ original_review = '''
 def root():
     return {'status': 'ok'}
 
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
+
 @app.post('/analyze_review')
-def analyze_review(review: str = Form(..., description="Review from marketplace")):
+def analyze_review(review: str = Form(..., description="Review from marketplace"),
+                   product_id: int = Form(..., description="Product ID")):
     thread_id = str(uuid.uuid4())
 
     config: RunnableConfig = {
@@ -208,11 +224,12 @@ def analyze_review(review: str = Form(..., description="Review from marketplace"
     try:
         result = graph.invoke({'original_review': review}, config=config)
 
-        return {
+        res = {
             'original_review': result.get('original_review'),
             'fixed_review': result.get('fixed_review'),
             'sentiment': result.get('sentiment'),
             'thread_id': thread_id,
+            'product_id': product_id,
             'good_points': {
                 'speed_of_delivery': result.get('good_speed_of_delivery'),
                 'price': result.get('good_price'),
@@ -230,6 +247,25 @@ def analyze_review(review: str = Form(..., description="Review from marketplace"
                 'functionality': result.get('bad_functionality'),
             }
         }
+
+        review_analysis_total.labels(
+            product_id=res['product_id'],
+            sentiment=res['sentiment'],
+            good_speed_of_delivery=res['good_points']['speed_of_delivery'],
+            good_price=res['good_points']['price'],
+            good_quality=res['good_points']['quality'],
+            good_good_looking=res['good_points']['good_good_looking'],
+            good_fit_description=res['good_points']['fit_description'],
+            good_functionality=res['good_points']['functionality'],
+            bad_speed_of_delivery=res['good_points']['speed_of_delivery'],
+            bad_price=res['bad_points']['price'],
+            bad_quality=res['bad_points']['quality'],
+            bad_good_looking=res['bad_points']['good_good_looking'],
+            bad_fit_description=res['bad_points']['fit_description'],
+            bad_functionality=res['bad_points']['functionality'],
+        ).inc()
+
+        return res
     finally:
         memory.delete_thread(thread_id)
 
