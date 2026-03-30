52ca75213aee604a4c8ceeba0a9494383a6db1b7
Ivan Remen <bhyk@yandex.ru>
2026-03-30 11:52:30 +0300

    Good points retrievers

--- src/app.py
+++ src/app.py
@@ -67,15 +67,40 @@ def sentiment_detection_call(state: State):
         'sentiment': sentiment,
     }
 
+class GoodPointsOfReview(BaseModel):
+    SpeedOfDelivery: bool = Field(description='Пользователю нравится скорость доставки')
+    Price: bool = Field(description='Пользователю нравится цена')
+    Quality: bool = Field(description='Пользователю нравится качество товара')
+    GoodLooking: bool = Field(description='Пользователю нравится дизайн и внешний вид товара')
+    FitDescription: bool = Field(description='Пользователю нравится то, что товар соответствует описанию товара')
+
+def good_points_detection_call(state: State):
+    print('Good points detection call')
+    system_message = SystemMessage(
+        f'''Ты должен найти в отзыве то, что нравится пользователю:
+        - Скорость доставки
+        - Качество товара
+        - Цена товара
+        - Дизайн и внешний вид товара
+        - Соответствие товара описанию
+
+         Игнорируй то, что не нравится.''')
+
+    user_message = HumanMessage(f"""Найди то, что нравится пользователю в этом отзыве.
+
+                                <review>{state['fixed_review']}</review""")
+    response = llm.with_structured_output(GoodPointsOfReview).invoke([system_message, user_message])
+    print(response)
 
 workflow = StateGraph(State)
 workflow.add_node('fix_review', fix_review_call)
 workflow.add_node('sentiment_detection', sentiment_detection_call)
+workflow.add_node('good_points_detection', good_points_detection_call)
 
 workflow.add_edge(START, "fix_review")
 workflow.add_edge('fix_review', 'sentiment_detection')
-workflow.add_edge('sentiment_detection', END)
-
+workflow.add_edge('sentiment_detection', 'good_points_detection')
+workflow.add_edge('good_points_detection', END)
 
 app = workflow.compile(checkpointer=memory)
 
@@ -84,6 +109,14 @@ original_review = '''
 снова на нем сошелся "свет клином", да еще и неандертальцы...как эхо романов про Северина Морозова, эхайна. (которые, кстати, написаны отменно). 
 ну и фирменное "давайте поделим на тысячу книг и много денежек заработаем", скоро будут каждую главу за деньги продавать. 
 несмотря на вышесказанное, вечерок скоротать можно.
+'''
+
+original_review = '''
+отличный, всё супер, спасибо продавцу за оперативную отправку, монитор пришёл раньше обещанной даты, битых пикселей нет, параметры в соответствии с заявленными  
+'''
+
+original_review = '''
+Достоинства:Внешний вид, светит ярко даже от двух ламп,соответствует описанию товара.Недостатки:Брак. Горят только два плафона. Лампы пробовали переставлять - эффект тот же. Электрик сказал, что еще и контакты не в порядке, все зачищал, переделывал что-то.Комментарий:Заморачиваться с возвратом за такие деньги не буду, освещение не основное. Пока так, но досадно.
 '''
 
 def main():
